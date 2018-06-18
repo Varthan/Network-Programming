513db199802ce3d8a701ff413a702c35f61d7419
Gowtham Varthan <36688223+rgvarthan@users.noreply.github.com>
2018-06-18 17:22:12 +0530

    To Align the code

--- Reverse ARP Server.java
+++ Reverse ARP Server.java
@@ -2,28 +2,28 @@ import java.net.*;
 import java.io.*;
 class  Server
 {
-public static void main(String args[])throws Exception
-{
-ServerSocket s=new ServerSocket (2000);
-Socket s0=s.accept();
-System.out.println("Connection Established");
-String ip[]={"10.1.6.1","213.51.16.98","26.54.32.10"};
-String mac[]={"A2:D3:56:3F:BC:9D","51:A6:6D:32:98:FA","24:DA:D8:9D:FF:01"};
-DataInputStream dis=new DataInputStream(s0.getInputStream());
-DataOutputStream dos=new DataOutputStream(s0.getOutputStream());
-int a=0;
-String str=dis.readLine();
-for(int i=0;i<mac.length;i++)
-{
- if(str.equals(mac[i]))
- {
-  a=1;
-  dos.writeBytes(ip[i]);
- }
-}
-if(a==0)
-System.out.println("MAC DOES NOT EXIST");
-System.out.println("	Operation Success");
-s0.close();
-}
+    public static void main(String args[])throws Exception
+    {
+        ServerSocket s=new ServerSocket (2000);
+        Socket s0=s.accept();
+        System.out.println("Connection Established");
+        String ip[]={"10.1.6.1","213.51.16.98","26.54.32.10"};
+        String mac[]={"A2:D3:56:3F:BC:9D","51:A6:6D:32:98:FA","24:DA:D8:9D:FF:01"};
+        DataInputStream dis=new DataInputStream(s0.getInputStream());
+        DataOutputStream dos=new DataOutputStream(s0.getOutputStream());
+        int a=0;
+        String str=dis.readLine();
+        for(int i=0;i<mac.length;i++)
+        {
+            if(str.equals(mac[i]))
+            {
+                a=1;
+                dos.writeBytes(ip[i]);
+            }
+        }
+        if(a==0)
+            System.out.println("MAC DOES NOT EXIST");
+        System.out.println("	Operation Success");
+        s0.close();
+    }
 }

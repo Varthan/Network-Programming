fca6fe12856fae91f2b2c4f1210115c79ecf0f72
Gowtham Varthan <36688223+rgvarthan@users.noreply.github.com>
2018-06-18 17:31:21 +0530

    To Align the code

--- Tcp_Ip Client Socket server.java
+++ Tcp_Ip Client Socket server.java
@@ -2,15 +2,17 @@ import java.net.*;
 import java.io.*;
 class  Server
 {
-public static void main(String args[])throws Exception
-{
-ServerSocket s=new ServerSocket (2000);
-Socket s0=s.accept();
-System.out.println("Connection Established");
-DataInputStream dis=new DataInputStream(s0.getInputStream());
-DataOutputStream dos=new DataOutputStream(s0.getOutputStream());
-String str=dis.readLine();
-System.out.println("CMD :"+str);
-s.close();
-}
+    public static void main(String args[])throws Exception
+    {
+        ServerSocket s=new ServerSocket (2000);
+        Socket s0=s.accept();
+        System.out.println("Connection Established");
+      
+        DataInputStream dis=new DataInputStream(s0.getInputStream());
+        DataOutputStream dos=new DataOutputStream(s0.getOutputStream());
+      
+        String str=dis.readLine();
+        System.out.println("CMD :"+str);
+        s.close();
+    }
 }

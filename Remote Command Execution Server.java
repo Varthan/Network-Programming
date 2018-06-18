e5628724937df5e2ccbb14c555328336a3008fe6
Gowtham Varthan <36688223+rgvarthan@users.noreply.github.com>
2018-06-18 17:20:40 +0530

    To Align the code

--- Remote Command Execution Server.java
+++ Remote Command Execution Server.java
@@ -2,18 +2,18 @@ import java.net.*;
 import java.io.*;
 class  Serv
 {
-public static void main(String args[])throws Exception
-{
-ServerSocket s=new ServerSocket (2000);
-Socket s0=s.accept();
-System.out.println("Connection Established");
-DataInputStream dis=new DataInputStream(s0.getInputStream());
-DataOutputStream dos=new DataOutputStream(s0.getOutputStream());
-String str=dis.readLine();
-Runtime r=Runtime.getRuntime();
-Process p=null;
-p=r.exec(str);
-System.out.println("Cmd: "+str);
-dos.writeBytes("Successfully Executed\n");
-}
+    public static void main(String args[])throws Exception
+    {
+        ServerSocket s=new ServerSocket (2000);
+        Socket s0=s.accept();
+        System.out.println("Connection Established");
+        DataInputStream dis=new DataInputStream(s0.getInputStream());
+        DataOutputStream dos=new DataOutputStream(s0.getOutputStream());
+        String str=dis.readLine();
+        Runtime r=Runtime.getRuntime();
+        Process p=null;
+        p=r.exec(str);
+        System.out.println("Cmd: "+str);
+        dos.writeBytes("Successfully Executed\n");
+    }
 }

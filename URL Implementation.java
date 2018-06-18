0c7f11a22cb25c3dc6419e2c4e068b08d4afe4cf
Gowtham Varthan <36688223+rgvarthan@users.noreply.github.com>
2018-06-18 17:39:00 +0530

    To Align the code

--- URL Implementation.java
+++ URL Implementation.java
@@ -3,21 +3,26 @@ import java.net.*;
 import java.util.*;
 class Url
 {
-public static void main(String args[])throws Exception
-{
-URL o=new URL("file:\\E:\\Student\\15bsr018\\gv.txt");
-URLConnection u=o.openConnection();
-System.out.println(new Date(u.getDate()));
-System.out.println(new Date(u.getLastModified()));
-System.out.println(o.getProtocol());
-System.out.println(o.getPath());
-System.out.println(o.getFile());
-System.out.println(u.getContentType());
-System.out.println(u.getContentLength());
-BufferedReader br=new BufferedReader(new InputStreamReader(u.getInputStream()));
-String s;
-while((s=br.readLine())!=null)
-System.out.println(s);
-br.close();
-}
+    public static void main(String args[])throws Exception
+    {
+        URL o = new URL("file:\\E:\\Java\\gv.txt");
+        URLConnection u = o.openConnection();
+      
+        System.out.println(new Date(u.getDate()));
+        System.out.println(new Date(u.getLastModified()));
+      
+        System.out.println(o.getProtocol());
+        System.out.println(o.getPath());
+        System.out.println(o.getFile());
+      
+        System.out.println(u.getContentType());
+        System.out.println(u.getContentLength());
+      
+        BufferedReader br = new BufferedReader(new InputStreamReader(u.getInputStream()));
+        
+        while((String s = br.readLine()) != null)
+            System.out.println(s);
+      
+        br.close();
+    }
 }
